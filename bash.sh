b5cf712d189894f4fae2923f8002e86ca0a5e171
kharlamov39 <kharlamov39@yandex.ru>
2024-04-16 11:18:34 +0300

    выгрузка

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
-# sudo docker-compose up --build -d
+# sudo docker compose up --build -d
 # echo 'First'
-# sudo docker-compose exec -d php-cli bash -c "chmod 777 -R /var/www && composer create-project laravel/laravel name_project"
+# sudo docker compose exec -d php-cli bash -c "chmod 777 -R /var/www && composer create-project laravel/laravel name_project"
 # echo 'Second'
 # sudo chmod 777 -R . 
 # echo 'Three'
