5a865ed4843f0ca3c80fff0f5a4da2b9a8b26bdf
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-18 10:19:36 +0300

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,17 @@
+#!/bin/sh
+cp -f .env.example .env
+echo 'Файл .env готов'
+sudo docker compose up
+echo 'Контейнеры запустились'
+sudo docker compose exec php-cli bash
+echo 'Вход в контейнер php-cli'
+composer update
+echo 'Папка vendor установлена'
+exit
+echo 'Вышли из контейнера php-cli'
+sudo chmod 777 -R .
+echo 'Установили права на проект'
+sudo docker compose exec node npm install
+echo 'Папка node modules установлена'
+npm run dev 
+echo 'Запуск Vite'
