a4087681fee5fe0ba4e02ea998cc15492a2afec5
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-18 10:58:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
+sudo chmod 777 -R .
+echo 'Установили права на проект'
 cp -f .env.example .env
 echo 'Файл .env готов'
 sudo docker compose up -d
 echo 'Контейнеры запустились'
 sudo docker compose exec php-cli composer update
 echo 'Папка vendor установлена'
-sudo chmod 777 -R .
-echo 'Установили права на проект'
-docker compose exec node npm install && echo 'Зависимости npm установлены успешно'
+sudo docker compose exec node npm install && echo 'Зависимости npm установлены успешно'
 
