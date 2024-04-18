e9f8013e420875454bc1a99a78bf5b1187c32e2a
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-18 10:37:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@ cp -f .env.example .env
 echo 'Файл .env готов'
 sudo docker compose up -d
 echo 'Контейнеры запустились'
-sudo docker compose exec php-cli bash
-echo 'Вход в контейнер php-cli'
-composer update
+sudo docker compose exec php-cli composer update
 echo 'Папка vendor установлена'
-exit
-echo 'Вышли из контейнера php-cli'
 sudo chmod 777 -R .
 echo 'Установили права на проект'
 sudo docker compose exec node npm install
